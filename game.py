d17099868d3cdfbac65c362a41b75d63d80757ad
Fedor Andreev <165912633+EnainRoflan@users.noreply.github.com>
2024-04-11 18:36:41 +0500

    теперь работает как швейцарские часы

--- game.py
+++ game.py
@@ -1,13 +1,13 @@
 from pygame import *
 
-window = display.set_mode((1000, 1200))
+window = display.set_mode((874, 682))
 display.set_caption('Ping-Pong')
 
 clock = time.Clock()
 
 background = transform.scale(
         image.load('table.png'),
-        (100, 1200)
+        (874, 682)
     )
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, speed, posX, posY, sizeX, sizeY):
@@ -24,19 +24,29 @@ class Players(GameSprite):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys_pressed[K_s] and self.rect.y > 1150:
+        if keys_pressed[K_s] and self.rect.y < 570:
             self.rect.y += self.speed
-#class Ball(GameSprite):
-#
+    def updatep2(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_DOWN] and self.rect.y < 570:
+            self.rect.y += self.speed
+player1 = Players('player.png', 10, 0, 341, 120, 120)
+player2 = Players('player.png', 10, 770, 341, 120, 120)
 
-
+init()
 run = True
 while run:
-    for event in event.get():
-        if event.type == QUIT:
+    for e in event.get():
+        if e.type == QUIT:
             run = False 
 
     window.blit(background, (0, 0))
+    player1.reset()
+    player1.update()
+    player2.reset()
+    player2.updatep2()
 
     clock.tick(40)
     display.update()
